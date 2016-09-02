5fcfc064a3c3404f6f5c0f99e833d2a342092bfc
Tom Aldcroft <taldcroft@gmail.com>
2016-09-02 13:36:36 -0400

    Refactor to collect tests in advance

--- run_tests.py
+++ run_tests.py
@@ -51,6 +51,11 @@ def get_options():
                       help=("Exclude tests that match comma-separated "
                             "list of glob pattern(s) (default='*_long')"),
                       )
+    parser.add_option("--collect-only",
+                      action="store_true",
+                      help=('Collect tests but do not run'),
+                      )
+
     return parser.parse_args()[0]
 
 
@@ -80,12 +85,6 @@ def box_output(lines, min_width=40):
     logger.info('')
 
 
-def get_packages():
-    with Ska.File.chdir(opt.packages_dir):
-        packages = [x for x in os.listdir('.') if os.path.isdir(x)]
-    return packages
-
-
 def include_test_file(package, test_file):
     path = os.path.join(package, test_file)
     include = any(fnmatch(path, x.strip()) for x in opt.include.split(','))
@@ -93,62 +92,97 @@ def include_test_file(package, test_file):
     return include and not exclude
 
 
-def run_tests(package):
-    # Collect test scripts in package and find the ones that are included
-    in_dir = os.path.join(opt.packages_dir, package)
-    with Ska.File.chdir(in_dir):
-        test_files = glob('test*.py') + glob('test*.sh')
-        include_test_files = [x for x in test_files if include_test_file(package, x)]
+def collect_tests():
+    """
+    Collect tests
+    """
+    with Ska.File.chdir(opt.packages_dir):
+        packages = [x for x in os.listdir('.') if os.path.isdir(x)]
 
-    skipping = '' if include_test_files else ': skipping - no included tests'
-    box_output(['package {}{}'.format(package, skipping)])
+    tests = {}
+    for package in packages:
+        tests[package] = []
 
-    # If no included tests then print message and bail out
-    if not include_test_files:
-        logger.info('')
-        return []
+        in_dir = os.path.join(opt.packages_dir, package)
+        out_dir = os.path.abspath(os.path.join(opt.outputs_dir, opt.outputs_subdir, package))
+        regress_dir = os.path.abspath(os.path.join(opt.regress_dir, opt.outputs_subdir, package))
 
-    # Make the output directory
+        with Ska.File.chdir(in_dir):
+            test_files = glob('test*.py') + glob('test*.sh') + glob('regress_files.py')
+            for test_file in test_files:
+                status = 'not run' if include_test_file(package, test_file) else 'skip'
+                interpreter = 'python' if test_file.endswith('.py') else 'bash'
+                test = {'file': test_file,
+                        'status': status,
+                        'interpreter': interpreter,
+                        'args': []}
+
+                if test_file == 'regress_files.py':
+                    test['args'] = ['--out-dir={}'.format(out_dir),
+                                    '--regress-dir={}'.format(regress_dir)]
+
+                tests[package].append(test)
+
+    print(tests)
+    return tests
+
+
+def get_out_dir(package):
+    """
+    """
     out_dir = os.path.join(opt.outputs_dir, opt.outputs_subdir, package)
+
+    if os.path.exists(out_dir):
+        answer = raw_input('Removing existing output directory {} (y/N)?'.format(out_dir))
+        if answer.lower() == 'y':
+            logger.info('Removing existing output directory {}'.format(out_dir))
+            shutil.rmtree(out_dir)
+
     if os.path.exists(out_dir):
         raise IOError('output dir {} already exists'.format(out_dir))
 
-    # Copy all files.  Excluded test scripts will be removed later.
+    os.makedirs(out_dir)
+
+    return out_dir
+
+
+def run_tests(package, tests):
+    # Collect test scripts in package and find the ones that are included
+    in_dir = os.path.join(opt.packages_dir, package)
+
+    include_tests = [test for test in tests if test['status'] != 'skip']
+    skipping = '' if include_tests else ': skipping - no included tests'
+    box_output(['package {}{}'.format(package, skipping)])
+
+    # If no included tests then print message and bail out
+    if not include_tests:
+        logger.info('')
+        return []
+
+    # Copy all files for package tests.
+    out_dir = os.path.join(opt.outputs_dir, opt.outputs_subdir, package)
     logger.info('Copying input tests {} to output dir {}'.format(in_dir, out_dir))
     shutil.copytree(in_dir, out_dir, symlinks=True, ignore=shutil.ignore_patterns('*~'))
 
-    # Make a symlink 'last' to the most recent directory
-    with Ska.File.chdir(opt.outputs_dir):
-        if os.path.exists('last'):
-            os.unlink('last')
-        os.symlink(opt.outputs_subdir, 'last')
-
     # Now run the tests and collect test status
-    statuses = []
     with Ska.File.chdir(out_dir):
-        for test_file in test_files:
-            if test_file not in include_test_files:
-                os.unlink(test_file)
-                statuses.append((test_file, 'skip'))
-                continue
+        for test in include_tests:
+            interpreter = test['interpreter']
 
-            interpreter = 'python' if test_file.endswith('.py') else 'bash'
-
-            logger.info('Running {} {} script'.format(interpreter, test_file))
-            logfile = Tee(test_file + '.log')
+            logger.info('Running {} {} script'.format(interpreter, test['file']))
+            logfile = Tee(test['file'] + '.log')
 
             try:
-                bash('{} {}'.format(interpreter, test_file), logfile=logfile)
+                cmd = ' '.join([interpreter, test['file']] + test['args'])
+                bash(cmd, logfile=logfile)
             except ShellError:
                 # Test process returned a non-zero status => Fail
-                statuses.append((test_file, 'fail'))
+                test['status'] = 'fail'
             else:
-                statuses.append((test_file, 'success'))
+                test['status'] = 'pass'
 
     box_output(['{} Test Summary'.format(package)] +
-               ['{:20s} {}'.format(test_file, status) for test_file, status in statuses])
-
-    return statuses
+               ['{:20s} {}'.format(test['file'], test['status']) for test in tests])
 
 
 def make_regress_files(package):
@@ -166,10 +200,44 @@ def make_regress_files(package):
     if os.path.exists(regress_files_path):
         with open(regress_files_path, 'r') as fh:
             regress_files = [x.strip() for x in fh.readlines() if x.strip()]
-        ska_test.runner.make_regress_files(regress_files, out_dir, regress_dir)
+        try:
+            ska_test.runner.make_regress_files(regress_files, out_dir, regress_dir)
+        except Exception as err:
+            logger.info('Error transferring regression files: {}'.format(err))
+
+
     elif os.path.exists(regress_files_path + '.py'):
         bash('python {}.py --out-dir={} --regress-dir={}'
              .format(regress_files_path, out_dir, regress_dir))
+
+
+def get_results_table(tests):
+    results = []
+    for package in sorted(tests):
+        for test in tests[package]:
+            results.append((package, test['file'], test['status']))
+    out = Table(rows=results, names=('Package', 'Script', 'Status'))
+    return out
+
+
+def make_test_dir():
+    test_dir = os.path.join(opt.outputs_dir, opt.outputs_subdir)
+    if os.path.exists(test_dir):
+        answer = raw_input('Removing existing output directory {} (y/N)?'.format(test_dir))
+        if answer.lower() == 'y':
+            logger.info('Removing existing output directory {}'.format(test_dir))
+            shutil.rmtree(test_dir)
+
+    if os.path.exists(test_dir):
+        raise IOError('output dir {} already exists'.format(test_dir))
+
+    os.makedirs(test_dir)
+
+    # Make a symlink 'last' to the most recent directory
+    with Ska.File.chdir(opt.outputs_dir):
+        if os.path.exists('last'):
+            os.unlink('last')
+        os.symlink(opt.outputs_subdir, 'last')
 
 
 def main():
@@ -179,19 +247,18 @@ def main():
     # Set up directories
     if opt.outputs_subdir is None:
         ska_version = bash('ska_version')[0]
-        opt.outputs_subdir = '{}-{}'.format(DateTime().fits[:19], ska_version)
+        opt.outputs_subdir = ska_version
 
-    os.makedirs(os.path.join(opt.outputs_dir, opt.outputs_subdir))
+    make_test_dir()
 
-    results = []
-    packages = get_packages()
-    for package in sorted(packages):
-        statuses = run_tests(package)
-        for test_file, status in statuses:
-            results.append((package, test_file, status))
-        make_regress_files(package)
+    tests = collect_tests()  # dict of (list of tests) keyed by package
 
-    results = Table(rows=results, names=('Package', 'Script', 'Status'))
+    if not opt.collect_only:
+        for package in sorted(tests):
+            run_tests(package, tests[package])  # updates tests[package] in place
+            make_regress_files(package)
+
+    results = get_results_table(tests)
     box_output(results.pformat())
 
 if __name__ == '__main__':
