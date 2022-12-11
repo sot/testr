b314d65b6e141130410871da7f075571c0994758
Tom Aldcroft <taldcroft@gmail.com>
2022-12-11 17:46:45 -0500

    Add duplicate_package_info function

--- testr/setup_helper.py
+++ testr/setup_helper.py
@@ -5,6 +5,7 @@ that ``python setup.py test`` runs tests via pytest.
 """
 
 import sys
+
 from setuptools.command.test import test as TestCommand
 
 __all__ = ['cmdclass', 'PyTest']
@@ -23,8 +24,10 @@ class PyTest(TestCommand):
 
     def run_tests(self):
         # Import here because outside the eggs aren't loaded
-        import pytest
         import shlex
+
+        import pytest
+
         from .runner import PYTEST_IGNORE_WARNINGS
 
         args = list(PYTEST_IGNORE_WARNINGS)
@@ -36,3 +39,26 @@ class PyTest(TestCommand):
 
 # setup() cmdclass keyword for testing with py.test
 cmdclass = {'test': PyTest}
+
+
+def duplicate_package_info(vals, name_in, name_out):
+    """
+    Duplicate a list or dict of values inplace, replacing ``name_in`` with ``name_out``.
+
+    Normally used in setup.py for making a namespace package that copies a flat one.
+    For an example see setup.py in the ska_sun or Ska.Sun repo.
+
+    :param vals: list or dict of values
+    :param name_in: string to replace at start of each value
+    :param name_out: output string
+    """
+    import re
+
+    for name in list(vals):
+        new_name = re.sub(f"^{name_in}", name_out, name)
+        if isinstance(vals, dict):
+            vals[new_name] = vals[name]
+        else:
+            vals.append(new_name)
+
+
